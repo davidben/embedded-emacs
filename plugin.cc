06537d41d8efff613b14f5ca71a60a1297613830
David Benjamin <davidben@mit.edu>
2010-10-20 16:09:26 -0400

    Initialize browser functions better

--- plugin.cc
+++ plugin.cc
@@ -1,6 +1,35 @@
-#define XP_UNIX 1
-#include "npapi-headers/npapi.h"
+#include "plugin.h"
+
+#include <algorithm>
+#include <cstring>
+
 #include "npapi-headers/npfunctions.h"
+
+NPNetscapeFuncs g_browser_functions = { 0 };
+
+namespace {
+
+NPError initializeBrowserFuncs(NPNetscapeFuncs* bFuncs)
+{
+    if (!bFuncs)
+        return NPERR_INVALID_FUNCTABLE_ERROR;
+
+    int major_version = bFuncs->version >> 8;
+    int minor_version = bFuncs->version & 0xff;
+
+    if (major_version != NP_VERSION_MAJOR)
+        return NPERR_INCOMPATIBLE_VERSION_ERROR;
+
+    // TODO: Assert support in minor_version for features we care
+    // about.
+
+    memcpy(&g_browser_functions, bFuncs,
+           std::min(sizeof(g_browser_functions), (size_t)bFuncs->size));
+
+    return NPERR_NO_ERROR;
+}
+
+}  // namespace
 
 NP_EXPORT(NPError) OSCALL NP_GetEntryPoints(NPPluginFuncs* pFuncs)
 {
@@ -13,9 +42,9 @@ NP_EXPORT(NPError) OSCALL NP_GetEntryPoints(NPPluginFuncs* pFuncs)
 NP_EXPORT(NPError) OSCALL NP_Initialize(NPNetscapeFuncs* bFuncs,
 					NPPluginFuncs* pFuncs)
 {
-    NPError err;
-
-    // TODO: save bFuncs
+    NPError err = initializeBrowserFuncs(bFuncs);
+    if (err != NPERR_NO_ERROR)
+        return err;
 
     // On Unix, it looks like NP_GetEntryPoints isn't called directly?
     // The prototype for NP_Initialize is different from everyone
