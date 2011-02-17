4d8f3cc36698e1d6f66a5db4e03a153900b747d4
David Benjamin <davidben@mit.edu>
2011-02-17 15:04:13 -0500

    Add XEmbedContainerPlugin
    This is a little helper plugin that exports an X11 window id. This
    should be safe to make public.

--- SConstruct
+++ SConstruct
@@ -4,3 +4,6 @@ env = Environment()
 env.SConscript('plugin/SConscript')
 env.Command('chrome/libemacs-npapi.so', 'plugin/libemacs-npapi.so',
             Copy("$TARGET", "$SOURCE"));
+env.Command('chrome/libembedded-emacs-container.so',
+            'plugin/libembedded-emacs-container.so',
+            Copy("$TARGET", "$SOURCE"));
---
+++ plugin/xembed_container_plugin.cc
@@ -0,0 +1,198 @@
+// Copyright (c) 2011 David Benjamin. All rights reserved.
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file.
+
+#include "npapi-cxx/browser.h"
+#include "npapi-cxx/plugin.h"
+#include "npapi-cxx/plugin_instance.h"
+#include "npapi-cxx/script_object.h"
+#include "util.h"
+
+namespace {
+
+NPIdentifier g_identifier_windowId = 0;
+
+class XEmbedContainerPlugin;
+class XEmbedContainerInstance;
+class XEmbedContainerObject;
+
+class XEmbedContainerPlugin : public npapi::Plugin {
+  public:
+    NPError init();
+
+    const char* getMIMEDescription();
+    NPError getValue(NPPVariable variable, void *value);
+    npapi::PluginInstance* createInstance(NPMIMEType pluginType, NPP npp,
+                                          uint16_t mode,
+                                          int16_t argc,
+                                          char *argn[], char *argv[],
+                                          NPSavedData* saved,
+                                          NPError* error);
+};
+
+class XEmbedContainerInstance : public npapi::PluginInstance {
+  public:
+    XEmbedContainerInstance(NPP npp);
+    ~XEmbedContainerInstance();
+
+    long windowId() { return window_id_; }
+
+    NPError setWindow(NPWindow* window);
+    NPError getValue(NPPVariable variable, void* value);
+  private:
+    XEmbedContainerObject *script_object_;
+    long window_id_;
+};
+
+class XEmbedContainerObject : public npapi::ScriptObject<XEmbedContainerObject> {
+  public:
+    XEmbedContainerInstance* pluginInstance();
+    bool hasProperty(NPIdentifier name);
+    bool getProperty(NPIdentifier name, NPVariant *result);
+  private:
+    friend class npapi::ScriptObject<XEmbedContainerObject>;
+    XEmbedContainerObject(NPP npp);
+    ~XEmbedContainerObject();
+};
+
+//////////////////////////////////////
+// XEmbedContainerPlugin
+//////////////////////////////////////
+
+NPError XEmbedContainerPlugin::init() {
+    int major_version, minor_version;
+    NPN_Version(NULL, NULL, &major_version, &minor_version);
+
+    // Require NPRuntime.
+    if (minor_version < NPVERS_HAS_NPRUNTIME_SCRIPTING)
+        return NPERR_INCOMPATIBLE_VERSION_ERROR;
+
+    // Require XEmbed support.
+    NPBool has_xembed = false;
+    NPError err = NPN_GetValue(NULL, NPNVSupportsXEmbedBool, &has_xembed);
+    if (err != NPERR_NO_ERROR || !has_xembed)
+        return NPERR_INCOMPATIBLE_VERSION_ERROR;
+
+    // Initialize one identifier I care about.
+    g_identifier_windowId = NPN_GetStringIdentifier("windowId");
+
+    return NPERR_NO_ERROR;
+}
+
+const char* XEmbedContainerPlugin::getMIMEDescription() {
+    return "application/x-embedded-emacs-container:"
+            ":"
+            "XEmbed container (Embedded Emacs)";
+}
+
+NPError XEmbedContainerPlugin::getValue(NPPVariable variable, void *value) {
+    NPError err = NPERR_NO_ERROR;
+    switch (variable) {
+        case NPPVpluginNameString:
+            *reinterpret_cast<const char **>(value) =
+                    "Embedded Emacs - XEmbed container";
+            break;
+        case NPPVpluginDescriptionString:
+            *reinterpret_cast<const char **>(value) =
+                        "Dummy plugin that exports X11 window id for a"
+                        " trusted plugin to embed Emacs into.";
+                break;
+            default:
+                err = Plugin::getValue(variable, value);
+        }
+    return err;
+}
+
+npapi::PluginInstance* XEmbedContainerPlugin::createInstance(
+    NPMIMEType pluginType, NPP npp, uint16_t mode,
+    int16_t argc, char *argn[], char *argv[],
+    NPSavedData* saved,
+    NPError* error) {
+    // TODO: Pass some of these arguments in??
+    return new XEmbedContainerInstance(npp);
+}
+
+
+//////////////////////////////////////
+// XEmbedContainerInstance
+//////////////////////////////////////
+
+XEmbedContainerInstance::XEmbedContainerInstance(NPP npp)
+    : PluginInstance(npp),
+      window_id_(0),
+      script_object_(NULL) {
+}
+
+XEmbedContainerInstance::~XEmbedContainerInstance() {
+    if (script_object_)
+        NPN_ReleaseObject(script_object_);
+}
+
+NPError XEmbedContainerInstance::setWindow(NPWindow* window) {
+    long window_id = reinterpret_cast<long>(window->window);
+    if (!window_id_) {
+        window_id_ = window_id;
+    } else {
+        // This really shouldn't happen.
+        if (window_id != window_id_)
+            return NPERR_GENERIC_ERROR;
+    }
+    return NPERR_NO_ERROR;
+}
+
+NPError XEmbedContainerInstance::getValue(NPPVariable variable, void* value) {
+    NPError err = NPERR_NO_ERROR;
+    switch (variable) {
+        case NPPVpluginNeedsXEmbed:
+            *reinterpret_cast<NPBool*>(value) = true;
+            break;
+        case NPPVpluginScriptableNPObject: {
+            if (!script_object_) {
+                script_object_ = XEmbedContainerObject::create(npp());
+                NPN_RetainObject(script_object_);
+            }
+            *reinterpret_cast<NPObject**>(value) =
+                    static_cast<NPObject*>(script_object_);
+            break;
+        }
+        default:
+            err = PluginInstance::getValue(variable, value);
+    }
+    return err;
+}
+
+//////////////////////////////////////
+// XEmbedContainerObject
+//////////////////////////////////////
+
+XEmbedContainerInstance* XEmbedContainerObject::pluginInstance() {
+    return static_cast<XEmbedContainerInstance*>(
+        npapi::ScriptObject<XEmbedContainerObject>::pluginInstance());
+}
+
+bool XEmbedContainerObject::hasProperty(NPIdentifier name) {
+    return (name == g_identifier_windowId);
+}
+
+bool XEmbedContainerObject::getProperty(NPIdentifier name, NPVariant *result) {
+    if (!hasProperty(name))
+        return false;
+    if (!pluginInstance())
+        return false;
+    INT32_TO_NPVARIANT(pluginInstance()->windowId(), *result);
+}
+
+XEmbedContainerObject::XEmbedContainerObject(NPP npp)
+    : npapi::ScriptObject<XEmbedContainerObject>(npp) {
+}
+
+XEmbedContainerObject::~XEmbedContainerObject() {
+}
+
+}  // namespace
+
+namespace npapi {
+Plugin* Plugin::createPlugin() {
+    return new XEmbedContainerPlugin();
+}
+}  // namespace npapi
