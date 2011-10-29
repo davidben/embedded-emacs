12ee7743c0c25996a01ed164433b76b3d97104c1
David Benjamin <davidben@mit.edu>
2011-10-29 18:03:41 -0400

    Add a copyright header to options.js

--- chrome/options.js
+++ chrome/options.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2011 David Benjamin. All rights reserved.
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file.
+
 var presets = [ {
     name: "Emacs",
     pattern: "emacs --parent-id $WINDOW --file $PATH",
